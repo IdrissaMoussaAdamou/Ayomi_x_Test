953408c26626da34b2f15e1fc3eccc93de5873f3
Idrissa Moussa Adamou <81566185+IdrissaMoussaAdamou@users.noreply.github.com>
2024-06-16 23:44:09 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
-#client = MongoClient("mongodb://localhost:27017/") # A Decommenter si vous voulez executer en local et commenter la ligne suivante
-client = MongoClient("mongodb://mongodb:27017/")  # Utiliser le nom de service Docker pour MongoDB
+client = MongoClient("mongodb://localhost:27017/") # A Decommenter si vous voulez executer en local et commenter la ligne suivante
+#client = MongoClient("mongodb://mongodb:27017/")  # Utiliser le nom de service Docker pour MongoDB
 db = client.operation_db
 
 def get_db():
